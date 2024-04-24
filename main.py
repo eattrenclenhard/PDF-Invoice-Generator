60743b09ef4cf54ad4052cdad8d18db9ae9f0ff8
Eat Tren Clen Hard <125723190+eattrenclenhard@users.noreply.github.com>
2024-04-24 23:57:47 +0800

    Final touches. Add header, total price, company name and logo

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ for f in filepaths:
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
+    grand_total = 0
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10)
         pdf.set_text_color(80, 80, 80)
@@ -38,5 +39,22 @@ for f in filepaths:
         pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    grand_total = df['total_price'].sum()
+
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=70, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, txt=str(grand_total), border=1, ln=1)
+
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.cell(w=30, h=8, txt=f"The total price is {grand_total}", ln=1)
+
+    pdf.set_font(family='Times', size=14, style='B')
+    pdf.cell(w=45, h=8, txt='Eat Tren Clen Hard')
+    pdf.image('logo.jpg', w=10)
 
     pdf.output(f'PDFs/{filename}.pdf')
